55f0944fb8081b20a4fad896ce9df1a45a4e06fe
CrazyPinguin <54096332+CrazyPinguin@users.noreply.github.com>
2021-03-24 23:40:35 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -8,26 +8,9 @@ myImage.onclick = function(){
     }
 }
 
-let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-function setUserName(){
-    
-    let myName = prompt("请输入你的名字。");
-    if(!myName){
-        setUserName();
-    }else{
-        localStorage.setItem("name",myName);
-        myHeading.textContent = "hello" + myName;
-    }
-}
-
-if(!localStorage.getItem("name")){
-    setUserName();
-}else{
-    let storedName = localStorage.getItem("name");
-    myHeading.textContent = "hello" + storedName;
-}
+myHeading.textContent = "i am 90h,please try your best!"
 
 myButton.onclick = function(){
-    setUserName();
+    alert("切个粑粑,自个儿找乐子去吧~");
 }
